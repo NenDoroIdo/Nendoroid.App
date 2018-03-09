9d35096753483e3eca1f436c1e80d942f11bc3aa
andy840119 <andy840119@gmail.com>
2018-03-09 17:25:03 +0800

    add appcenter support

--- Form/App.xaml.cs
+++ Form/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Form.Pages.Search.Main;
 using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Form
 {
@@ -15,6 +18,11 @@ namespace Form
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=57193881-deaa-4419-b401-f89675081968;" +
+                            "uwp=411a559a-22c5-4918-9be4-9f92f7a99aa4;" +
+                            "android=7b6bdb59-4ba0-4360-94a9-1a30c40b80d8;",
+                typeof(Analytics), typeof(Crashes));
+
         }
 
         protected override void OnSleep()
