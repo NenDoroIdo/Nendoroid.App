9a66c42cfaeb7c3fa53b64a3b04303e9fb063a48
andy840119 <andy840119@gmail.com>
2018-03-09 16:35:01 +0800

    add cell for mainSearchPage

---
+++ Form/Pages/Search/Main/Cell/AccessoryTypeCell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Form.Pages.Search.Main.Cell
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AccessoryTypeCell : ContentView
+	{
+		public AccessoryTypeCell ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
